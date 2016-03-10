3404a06ba67ed503ede9b4136d44d43e6061c425
shineangelic <shineangelic@gmail.com>
2016-03-10 23:26:58 +0100

    so  many changes. Refactored packages, externalized lib, minor DemoApp Enhancements wip Maven publish

---
+++ tagviewlib/src/main/java/Tag/OnTagClickListener.java
@@ -0,0 +1,8 @@
+package Tag;
+
+/**
+ * listener for tag delete
+ */
+public interface OnTagClickListener {
+	void onTagClick(Tag tag, int position);
+}
---
+++ tagviewlib/src/main/java/Tag/OnTagDeleteListener.java
@@ -0,0 +1,8 @@
+package Tag;
+
+/**
+ * listener for tag delete
+ */
+public interface OnTagDeleteListener {
+	void onTagDeleted(TagView view, Tag tag, int position);
+}
